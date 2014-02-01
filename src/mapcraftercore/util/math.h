e481e9f5de6e10d60df84d14ede2c04e09dc8f1d
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-02-01 12:36:26 +0100

    Renamed some math functions to keep consistency.
    automatic commit by git-black, original commits:
      2bbe6ce5b7b2638bf75891d29c5c82f3e1a3fa9f
      bcb5283dcae1030d2a34c45547e03cdb7d1af474

--- src/mapcraftercore/util/math.h
+++ src/mapcraftercore/util/math.h
@@ -30,8 +30,7 @@ namespace util {
  * Example: 0b0010 >> 2 = 0b1000
  */
 template <typename T> T rotateShiftRight(T x, int m, int n) {
-T rotateShiftRight(T x, int m, int n) {
-	return (x >> m) | ((x & ((1 << m) - 1)) << (n - m));
+    return (x >> m) | ((x & ((1 << m) - 1)) << (n - m));
 }
 
 /**
@@ -39,8 +38,7 @@ T rotateShiftRight(T x, int m, int n) {
  * Example: 0b0100 << 2 = 0b0001
  */
 template <typename T> T rotateShiftLeft(T x, int m, int n) {
-T rotateShiftLeft(T x, int m, int n) {
-	return ((x << m) & ((1 << n) - 1)) | (x >> (n - m));
+    return ((x << m) & ((1 << n) - 1)) | (x >> (n - m));
 }
 
 /**
