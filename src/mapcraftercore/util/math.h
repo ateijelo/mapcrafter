d4864aeb86ff706ebcadaec25d63bcccd68a9949
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-05-11 23:02:28 +0200

    Small typo fixed, little tweaks.
    automatic commit by git-black, original commits:
      0908d1edfba51df8ea36e20c05bc4bfcf7d65659
      9a901e775abc8550ab40353130e37a070385954e

--- src/mapcraftercore/util/math.h
+++ src/mapcraftercore/util/math.h
@@ -62,8 +62,7 @@ template <typename T> bool floatingPointEquals(T a, T b, T epsilon = 0.0001) {
  */
 template <unsigned long N> struct binary {
     // prepend higher bits to lowest bit
-	// prepend higher bits to lowest bit
-	static const unsigned long value = binary<N / 10>::value << 1 | N % 10;
+    static const unsigned long value = binary<N / 10>::value << 1 | N % 10;
 };
 
 template <unsigned long N> const unsigned long binary<N>::value;
