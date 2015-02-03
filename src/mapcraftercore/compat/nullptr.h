0d4691eb64cfc403962e299dd18f46c59d232a17
m0r13 <moritz.hilscher@gmail.com>
2015-02-03 14:33:45 +0100

    Damn, forgot to add nullptr.h
    automatic commit by git-black, original commits:
      fd5002da0f2b27ea35057e6668c232856b8d84a8

--- src/mapcraftercore/compat/nullptr.h
+++ src/mapcraftercore/compat/nullptr.h
@@ -25,18 +25,23 @@
 #ifndef HAVE_NULLPTR
 // official workaround for nullptr
 // see also http://stackoverflow.com/questions/2419800/can-nullptr-be-emulated-in-gcc
-const                        // this is a const object...
-class {
-public:
-  template <class T>          // convertible to any type
-    operator T*() const      // of null non-member
-    { return 0; }            // pointer...
-  template <class C, class T> // or any type of null
-    operator T C::*() const  // member pointer...
-    { return 0; }
-private:
-  void operator&() const;    // whose address can't be taken
-} nullptr = {};              // and whose name is nullptr
+const // this is a const object...
+    class {
+  public:
+    template <class T>   // convertible to any type
+    operator T *() const // of null non-member
+    {
+        return 0;
+    }                           // pointer...
+    template <class C, class T> // or any type of null
+    operator T C::*() const     // member pointer...
+    {
+        return 0;
+    }
+
+  private:
+    void operator&() const; // whose address can't be taken
+} nullptr = {};             // and whose name is nullptr
 #endif
 
 #endif /* COMPAT_NULLPTR_H_ */
