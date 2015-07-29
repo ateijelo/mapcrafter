7d873f947fe82294a13c3da43d58d2b8f7bc28eb
Moritz Hilscher <moritz.hilscher@gmail.com>
2015-07-29 20:45:04 +0200

    Implemented option to control water opacity. Not documented yet. Fixed isometric preblit water.
    automatic commit by git-black, original commits:
      160768714fb750a850c28504e1fe2f4fe4ceb6c3
      3b7938550fb47c47c55d05772e6e07f2f2cc19d6
      564ac2ff17d46e91ae11e9d1a5ebecc3d6cf6762
      73857120a1509ddd20dc8262a0dc6b83804dbded
      80f00678727ffe2bde05fe95257dcf9c02d42739
      96195b11d93daca8ea26c472eec27c7b49618c25
      a98c904f6208270ad25a3e4d7ed06dafe848704e
      b210c91ccd4d27c641f94588b6b31350ac5c4093
      d009742646448d7c9ef5647f64ec06dba2f9d7a2
      e4a01f0823ddc5f61afecf584e97f3c87b16b7b2

--- src/mapcraftercore/renderer/textureimage.cpp
+++ src/mapcraftercore/renderer/textureimage.cpp
@@ -93,75 +93,7 @@ bool TextureImage::load(const std::string &path, int size, int blur, double wate
     return true;
 }
 
-TextureImage::TextureImage(const std::string& name)
-	: name(name), frame_count(1) {
-}
-
-TextureImage::~TextureImage() {
-}
-
-bool TextureImage::load(const std::string& path, int size, int blur, double water_opacity) {
-	// at first try to load the texture file
-	if (!original.readPNG(path + "/" + name + ".png")) {
-		// make sure the texture image does not have zero dimension
-		// even if the texture does not exist / is broken
-		this->setSize(size, size);
-		original = original_resized = *this;
-		return false;
-	}
-
-	// check if this is an animated texture, calculate how many frames it has
-	// also make sure there are exactly n frames, so height mod width = 0
-	if ((original.getHeight() % original.getWidth()) != 0) {
-		LOG(WARNING) << "Texture '" << name << "' has odd size: " << original.getWidth()
-				<< "x" << original.getHeight();
-	}
-	frame_count = original.getHeight() / original.getWidth();
-
-	// now resize the texture image
-	// resize some textures with the nearest neighbor interpolation:
-	// - transparent leaves
-	// - redstone
-	// because smooth interpolation causes here half-transparent pixel which are not
-	// good for performance and makes redstone looking not very good and identifiable,
-	// instead of that the nearest neighbor interpolation preserves the pixelated
-	// style of the textures and prevents fuzziness when resizing
-	if ((util::startswith(name, "leaves") && !util::endswith(name, "opaque"))
-		|| util::startswith(name, "redstone_dust"))
-		original.resize(original_resized, size, size * frame_count, InterpolationType::NEAREST);
-	else
-		original.resize(original_resized, size, size * frame_count);
-
-	int width = original_resized.getWidth();
-	int height = original_resized.getHeight();
-	// apply a blur to the texture if wanted
-	// this is useful if you use small texture sizes (< 6 maybe) to prevent grainy textures
-	if (blur != 0) {
-		for (int i = 0; i < frame_count; i++) {
-			RGBAImage frame;
-			// process every frame individually
-			original_resized.clip(0, width * i, width, width).blur(frame, blur);
-			original_resized.simpleBlit(frame, 0, width * i);
-		}
-	}
-
-	// apply opacity factor to water textures
-	if (util::startswith(name, "water_") && water_opacity != 1.0) {
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				RGBAPixel& pixel = original_resized.pixel(x, y);
-				uint8_t alpha = std::min(255.0, rgba_alpha(pixel) * water_opacity);
-				pixel = rgba(rgba_red(pixel), rgba_green(pixel), rgba_blue(pixel), alpha);
-			}
-		}
-	}
-
-	// assign actual texture to parent RGBAImage object
-	// uses first frame if this is an animated texture
-	this->setSize(size, size);
-	this->simpleAlphaBlit(getFrame(0), 0, 0);
-	return true;
-}
+const std::string &TextureImage::getName() const { return name; }
 
 const std::string& TextureImage::getName() const {
 	return name;
