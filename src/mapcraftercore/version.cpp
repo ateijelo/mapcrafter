8460acc92995a3d043a77c1f22299a71f7060792
Andy Teijelo <ateijelo@gmail.com>
2025-05-12 18:41:32 -0400

    missed step

--- src/mapcraftercore/version.cpp
+++ src/mapcraftercore/version.cpp
@@ -3,5 +3,5 @@
 namespace mapcrafter {
 	const char* MINECRAFT_VERSION = "1.20";
 	const char* MAPCRAFTER_VERSION = "3.0";
-	const char* MAPCRAFTER_GITVERSION = "v.2.3.1-1049-g178a2158";
+	const char* MAPCRAFTER_GITVERSION = "v.2.3.1-1051-g3c1b5b6b";
 };
